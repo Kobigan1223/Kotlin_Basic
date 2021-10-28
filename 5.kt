548d64e8fe16ee59d7245ec07d9c44ec742a353c
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-10-29 00:04:18 +0530

    Updated

---
+++ 5.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val name = "Rover"
+    val age = 5 * 365
+    println("Happy Birthday, ${name}!")
+    
+    // Let's print a cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    
+    // This prints an empty line.
+    println("")
+    println("You are already ${age} days old, ${name}!")
+    println("${age} is the very best age to celebrate!")
+}
