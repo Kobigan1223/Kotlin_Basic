9886739898d2a7db248e3ee784daa552b0085b53
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-10-29 00:04:56 +0530

    Updated

---
+++ 7.kt
@@ -0,0 +1,16 @@
+
+fun main() {
+    val border = "`-._,-'"
+    val timesToRepeat = 4
+    printBorder(border, timesToRepeat)
+    
+    println("Happy Birthday, Jhansi!")
+     printBorder(border, timesToRepeat)
+}
+
+fun printBorder(border: String, timesToRepeat: Int) {
+    repeat(timesToRepeat) {
+        print(border)
+    }
+    println()
+}
